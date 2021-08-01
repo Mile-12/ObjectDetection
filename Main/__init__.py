abcfdc1bb511b28b3c2695f8b2b002c87d2438ab
NatiTsegaye <86118205+NatiTsegaye@users.noreply.github.com>
2021-08-01 07:20:19 +0300

    Add files via upload

---
+++ Main/__init__.py
@@ -0,0 +1,20 @@
+"""This is init module."""
+from flask_restful import Api
+from flask import Flask, Response, request, jsonify
+from flask_bcrypt import Bcrypt
+import os
+from Main.Routes.routes import initialize_routes
+from flask_cors import CORS
+# Place where app is defined
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'Main/api/uploads'
+api = Api(app)
+CORS(app, resources={r'/api/*':{"origins": "*"}},allow_headers=[
+    "Content-Type", "Authorization", "Access-Control-Allow-Methods"])
+#CORS(app)
+app.config['MONGODB_SETTINGS'] = {
+    'host': 'mongodb://localhost/API-DB'
+}
+
+initialize_routes(api)
+bcrypt = Bcrypt(app)
